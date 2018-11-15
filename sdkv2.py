4b882f7a754f9fa81a4b8639f3cd29a3b424f685
liebian <44887813+liebian@users.noreply.github.com>
2018-11-15 14:17:35 +0800

    Update sdkv2.py

--- sdkv2.py
+++ sdkv2.py
@@ -1,1 +1,18 @@
+#! -*- coding:utf-8 -*-
+import os
+import sys
 
+class DouyinSign(object):
+    def __init__(self):
+        pass
+    
+    """
+        com.ss.android.common.applog.k.a(byte[] barr) 方法
+    """
+    def evident_6041(self, input_bytes):
+        if input_bytes is None:
+            return None
+        else:
+            pass
+            
+        
