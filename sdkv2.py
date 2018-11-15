f335c432bdbc2d01c1a0e3220ed621f74b1170db
liebian <44887813+liebian@users.noreply.github.com>
2018-11-15 17:36:17 +0800

    Update sdkv2.py

--- sdkv2.py
+++ sdkv2.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 class AbstractDouyinSign(object):
-    def work(self, url, params):
+    def work(self, url):
         pass
 
 '''
@@ -13,9 +13,12 @@ class OnlineDouyinSign(AbstractDouyinSign):
     def __init__(self):
         pass
     
-    def work(self, url, params):
-        pass
-
+    def work(self, url):
+        return url
+    
+    
+        
+     
 '''
     离线服务生成签名, 算法只分析签名逻辑, 暂不提供key
 '''
@@ -26,9 +29,9 @@ class OfflineDouyinSign(AbstractDouyinSign):
         self.HIDDEN_KEY3 = HK3
         self.HIDDEN_KEY4 = HK4
     
-    """
+   `'''
         com.ss.android.common.applog.k.a(byte[] barr) 方法
-    """
+    '''
     def evident_6041(self, input_bytes):
         if input_bytes is None:
             return None
@@ -36,23 +39,23 @@ class OfflineDouyinSign(AbstractDouyinSign):
             pass
             
         
-    """
+    '''
         libcms.so sub_26750逻辑
-    """
+    '''
     def sub_26750(self, ts, arr, devid):
         pass
     
     
-    """
+    '''
         libcms.so sub_2f1c8逻辑
-    """
+    '''
     def sub_2f1c8(self, barr):
         pass
     
     
-    """
+    '''
         生成最终的带签名url
-    """
+    '''
     def work(self, url, param):
         pass
     
